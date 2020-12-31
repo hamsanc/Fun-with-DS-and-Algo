4e4877baf29ebb4c6364f34fd074e816c1c491c6
Hamsa <nchamsa@gmail.com>
2020-12-31 12:53:45 +0530

    Removed old files and worked on folder structure

---
+++ Coursera/Algorithms_II/boggle/BoggleSolver.java
@@ -0,0 +1,31 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+import java.util.ArrayList;
+
+public class BoggleSolver {
+    // Initializes the data structure using the given array of strings as the dictionary.
+    // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
+    public BoggleSolver(String[] dictionary) {
+
+    }
+
+    // Returns the set of all valid words in the given Boggle board, as an Iterable.
+    public Iterable<String> getAllValidWords(BoggleBoard board) {
+        return new ArrayList<String>() {
+        };
+    }
+
+    // Returns the score of the given word if it is in the dictionary, zero otherwise.
+    // (You can assume the word contains only the uppercase letters A through Z.)
+    public int scoreOf(String word) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
